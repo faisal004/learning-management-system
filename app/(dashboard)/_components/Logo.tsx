42834e7a4f3def054d488ae4dfdd0c7039a541a2
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-10-18 11:24:41 +0530

    Css fixed

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Logo = () => {
   return (
     <div>
-      <Image height={13} width={13} alt="Logo" src="/logo.svg" />
+      <Image height={40} width={40} alt="Logo" src="/logo.svg" />
     </div>
   )
 }
