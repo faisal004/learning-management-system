0cc37c58504f3b076a0f6caf18aac4c8e98a12bf
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-10-18 11:25:07 +0530

    Css added

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -39,13 +39,14 @@ const CreatePage = () => {
     try {
       const response = await axios.post('/api/courses', values)
       router.push(`/teacher/courses/${response.data.id}`)
+      toast.success('Course Created')
     } catch (error) {
       toast.error('Something went wrong')
     }
   }
   return (
-    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
-      <div>
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center md:h-full p-6 font-semibold ">
+      <div className="bg-gray-300 p-4 rounded-lg border-2 border-black">
         <h1 className="text-2xl">Name your course</h1>
         <p className="text-sm text-slate-600">
           What would you like to name your course? Don&apos;t worry,you can
