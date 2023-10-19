1b1373c89c90c6c3fdc40f6c0dbac6b95aca6bde
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-10-19 15:28:29 +0530

    Enhancing user experience

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignIn />;
+
+  return <div className="flex flex-col md:flex-row items-center">
+  <SignIn />
+  <div className="bg-slate-300 p-3 rounded-md flex flex-col text-center font-serif">
+    Don&apos;t want to signin? try this with test user
+    <span className="text-2xl">test99user88@gmail.com</span>
+    <span className="text-2xl">Password - test99user88</span>
+  </div>
+  </div>
 }
