a527ae77db722ce0254c2dcea3e2d4759c839432
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-10-23 10:20:24 +0530

    Final

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Logo = () => {
   return (
     <div>
+      <Link href={"/"}>
       <Image height={40} width={40} alt="Logo" src="/logo.svg" />
+      
+      </Link>
+      
     </div>
   )
 }
