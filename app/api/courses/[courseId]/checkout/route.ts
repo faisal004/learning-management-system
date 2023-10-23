79dd72716ff5405d5f91825e7edc5c353e4a6098
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-10-23 10:19:27 +0530

    more api route added

---
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -0,0 +1,58 @@
+
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function POST(
+    req: Request,
+    { params }: { params: { courseId: string } }
+) {
+    try {
+        const user = await currentUser();
+
+        if (!user || !user.id || !user.emailAddresses?.[0]?.emailAddress) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+
+        const course = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                isPublished: true,
+            }
+        });
+
+        if (!course) {
+            return new NextResponse("Course not found or not published", { status: 404 });
+        }
+
+
+        const purchase = await db.purchase.findUnique({
+            where: {
+                userId_courseId: {
+                    userId: user.id,
+                    courseId: params.courseId
+                }
+            }
+        });
+
+        if (purchase) {
+            return new NextResponse("Already purchased", { status: 400 });
+        }
+
+        const createPurchase = await db.purchase.create({
+            data: {
+                userId: user.id,
+                courseId: params.courseId,
+
+            }
+        });
+
+
+        return new NextResponse("Purchase successful", { status: 200 });
+    } catch (error) {
+        console.log("[COURSE_ID_CHECKOUT]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
